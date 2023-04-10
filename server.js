9d8f2679e86dc0fdb9781fb0d4a4b4e377ac3f96
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-04-10 19:15:46 -0400

    very basic initiation

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
+// Require Libraries
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+// Middleware
+const handlebars =  require('express-handlebars');
+const hbs = handlebars.create({
+    // Specify helpers which are only registered on this instance.
+    helpers: {
+        foo() { return 'FOO!'; },
+        bar() { return 'BAR!'; }
+    }
+});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
 app.listen(PORT, () =>
-  console.log(`Example app listening on port ${PORT}!`),
+  console.log(`Nodeddit app listening on port ${PORT}!`),
 );
